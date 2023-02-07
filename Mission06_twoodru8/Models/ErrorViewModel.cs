42305c4ddffc08a6319e9553ae9611bc1b855abc
tannerwoodrum <tannerwoodrum@TANNERWOODR8C05>
2023-02-07 11:21:51 -0700

    Add project files.

---
+++ Mission06_twoodru8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission06_twoodru8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
